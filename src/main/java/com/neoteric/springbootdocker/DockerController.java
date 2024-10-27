e57a32b2d6c6bf375883fa0af47cd46b45b84ef8
vijaybareddy <vijaybareddy143@gmail.com>
2024-10-27 10:40:13 +0530

    hi vijay

--- src/main/java/com/neoteric/springbootdocker/DockerController.java
+++ src/main/java/com/neoteric/springbootdocker/DockerController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerController {
     @GetMapping(value = "/docker")//hi vijay
     public String getDocker(){
-        return "Hello from docker";
+        return "Hello from to docker";
     }
 }
